03f82df3333220c6b93cdddbffcd1a8446aa7e25
Djordje Maric <djordje.maric@quantox.com>
2019-01-11 14:46:13 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Welcome back changes, test
+          </p>
+          <p>
+            test
           </p>
           <a
             className="App-link"
@@ -17,7 +20,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React madafaka
           </a>
         </header>
       </div>
